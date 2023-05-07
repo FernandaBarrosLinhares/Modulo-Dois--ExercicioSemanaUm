0498c99e4408236d30aaaee7460331bfd25716e4
FernandaBarrosLinhares <124844643+FernandaBarrosLinhares@users.noreply.github.com>
2023-05-07 18:47:59 -0300

    Add files via upload

---
+++ ExercicioQuatro.java
@@ -0,0 +1,20 @@
+/*Faça dentro do escopo do método main uma rotina através de laço de
+ repetição while um programa que receberá nomes de alunos
+ até que seja digitado "fim”
+ */
+
+import java.util.Scanner;
+
+public class ExercicioQuatro {
+
+    public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+        String nomeDigitado;
+        System.out.println("Digite o nome do aluno");
+        nomeDigitado = scanner.nextLine();
+    //Imprime na tela o nome que foi digitado
+        System.out.println();
+
+    }
+}
